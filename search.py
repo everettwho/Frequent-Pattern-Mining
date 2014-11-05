6f2b026b5dc2c632f715cd09a611c0e49b9fbda0
ChickanPotPai <everettwho@gmail.com>
2014-11-05 16:31:27 -0600

    Added description

--- search.py
+++ search.py
@@ -1,7 +1,10 @@
+# test script
+
 search_for = ['407', '382', '225']
 count = 0
 
 data_file = open("data-assign3/topic-0.txt")
+
 for line in data_file.readlines():
     data = line.rstrip().split(' ')
     flag = True
